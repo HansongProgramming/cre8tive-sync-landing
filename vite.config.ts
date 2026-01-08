b72a45493d569841a93d1ca69086642bc86b17c7
HansongProgramming <floreshansharoldlee@gmail.com>
2026-01-08 23:56:40 +0800

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/",
+  base: "/cre8tive-sync-landing/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
