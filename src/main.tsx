0b0354ac23f76389c859725ea8e26f1bcac8b827
HansongProgramming <floreshansharoldlee@gmail.com>
2026-02-15 22:13:07 +0800

    added new page and route

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./App.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import { ServiceQuote } from "./components/ServiceQuote";
+import "./App.css";
+import { createRoot as reactCreateRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/service-quota" element={<ServiceQuote />} />
+		</Routes>
+	</BrowserRouter>
+);
+function createRoot(element: HTMLElement) {
+    return reactCreateRoot(element);
+}
